cb556fe357487523126ffd73460dcd45bd67fe81
itsbrijeshio <bk6500416@gmail.com>
2025-07-23 10:49:57 +0530

    fix: update env.ts  and frontend origin

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,20 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes";
-import {setupSwagger} from "./docs/swagger"
+import { setupSwagger } from "./docs/swagger";
 import { errorHandler, rateLimiter } from "./middlewares";
+import { env } from "./config";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    origin: env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(rateLimiter());
--- src/config/env.ts
+++ src/config/env.ts
@@ -14,6 +14,7 @@ const env = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL as string,
+  FRONTEND_URL: process.env.FRONTEND_URL as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
   ENCRYPTION_SECRET: process.env.ENCRYPTION_SECRET as string,
